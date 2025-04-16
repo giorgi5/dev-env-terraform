dca88f8841721a0a28044f440528243c1f0038ee
giorgi5 <giorgirex7@gmail.com>
2025-04-16 17:43:51 -0400

    data source depend on module

--- eks-module/data.tf
+++ eks-module/data.tf
@@ -8,12 +8,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = data.aws_eks_cluster.cluster.name
 }
 
-data "aws_iam_openid_connect_provider" "oidc" {
-  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-
-  depends_on = [aws_eks_cluster.k8scluster]
-}
-
 data "external" "oidc_thumbprint" {
   program = ["bash", "${path.module}/get_oidc_thumbprint.sh"]
 
--- eks-module/get_oidc_thumbprint.sh
+++ eks-module/get_oidc_thumbprint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 OIDC_URL=$(jq -r '.oidc_url' <&0)
-THUMBPRINT=$(echo | openssl s_client -connect ${OIDC_URL#https://}:443 -servername ${OIDC_URL#https://} 2>/dev/null \
-  | openssl x509 -fingerprint -noout \
-  | cut -d"=" -f2 \
-  | tr -d ':')
+HOST=$(echo "$OIDC_URL" | sed 's|https://||')
+
+THUMBPRINT=$(echo | \
+  openssl s_client -servername "$HOST" -connect "$HOST:443" 2>/dev/null | \
+  openssl x509 -fingerprint -noout -sha1 | \
+  cut -d'=' -f2 | tr -d ':' | tr 'A-Z' 'a-z')
 
 jq -n --arg thumbprint "$THUMBPRINT" '{"thumbprint":$thumbprint}'
